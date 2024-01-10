7bec75891a722c56b05ff546fc91ce944f6d4f17
Jason Alberto <alberto.jason@gmail.com>
2024-01-10 18:42:39 -0500

    Add game prompt and created inputDirection method and started a runGame method, moved the game rules into a method called gameRules

--- backend-career-path/find-your-hat/main.js
+++ backend-career-path/find-your-hat/main.js
@@ -10,7 +10,44 @@ const pathCharacter = '*';
 class Field {
   constructor(field) {
     this.field = field;
+    this.locationX = 0;
+    this.locationY = 0;
   }
+
+  runGame() {
+    this.gameRules();
+    this.print();
+    this.inputDirection();
+  }
+
+  inputDirection() {
+    const direction = prompt('Which way? ');
+
+    switch(direction) {
+      case 'r': 
+        this.locationX += 1;
+        break;
+      case 'l':
+        this.locationX -= 1;
+        break;
+      case 'u': 
+        this.locationY -= 1;
+        break;
+      case 'd':
+        this.location += 1;
+        break;
+      default:
+        console.log('Please enter r, l, d or u.');
+        this.inputDirection();
+        break;
+    }
+  }
+
+  gameRules() {
+    process.stdout.write("You(*) need to find your hat(^)!!! \nMake sure not to fall in a hole(O) and to stay on the field(░). \n'r' moves right  \n'u' moves up  \n'd' moves down  \n'l' moves left \n\nGAME FIELD:\n")
+  }
+
+
 
   print() {
     const printField = this.field.map(row => {
@@ -26,11 +63,5 @@ const myField = new Field([
   ['░', '^', '░'],
 ]);
 
-process.stdout.write(`You(*) need to find your hat(^)!!!
-Make sure not to fall in a hole(O). 
-'r' moves right
-'u' moves up
-'d' moves down
-'l' moves left
-Where do you want to move?\n`)
-myField.print();
+
+myField.runGame();
