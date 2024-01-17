e42b083fbeed2da8f2e1ec3ece6267dfef8c2597
Jason Alberto <alberto.jason@gmail.com>
2024-01-16 19:44:38 -0500

    Add endpoint for /api/quotes

--- backend-career-path/quote-api-starting/modules/quotes.js
+++ backend-career-path/quote-api-starting/modules/quotes.js
@@ -5,6 +5,10 @@ const { quotes } = require('../data');
 const { getRandomElement } = require('../utils');
 quoteRoute = express.Router();
 
+quoteRoute.get('/', (req, res, next) => {
+  res.send(quotes);
+});
+
 quoteRoute.get('/random', (req, res, next) => {
   const randomQuote = getRandomElement(quotes);
   if(randomQuote) {
