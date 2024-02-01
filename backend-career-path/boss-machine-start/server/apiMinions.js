70843a0807ee36a9878c060b94f49104bfe8949f
Jason Alberto <alberto.jason@gmail.com>
2024-02-01 15:33:45 -0500

    Setup post minions route to add a new minion

--- backend-career-path/boss-machine-start/server/apiMinions.js
+++ backend-career-path/boss-machine-start/server/apiMinions.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const minionRouter = express.Router();
-const { getAllFromDatabase } = require('./db')
+const { getAllFromDatabase, addToDatabase } = require('./db')
 
 const modelMinions = 'minions';
 
 minionRouter.get('/', (req, res, next) => {
   const allMinions = getAllFromDatabase(modelMinions);
   res.send(allMinions);
-})
+});
+
+minionRouter.post('/', (req, res, next) => {
+  const newMinion = req.body;
+  if(!newMinion) {
+    res.status(400).send();
+  } else {
+    const createdMinion = addToDatabase(modelMinions, newMinion);
+    res.status(201).send(createdMinion);
+  }
+});
 
 module.exports = minionRouter;
