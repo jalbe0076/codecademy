a6c672d43afe4d8f96955b54f9de175a6f5b14df
Jason Alberto <alberto.jason@gmail.com>
2024-02-01 17:38:53 -0500

    Setup post for meetings route to create a new meeting

--- backend-career-path/boss-machine-start/server/apiMeetings.js
+++ backend-career-path/boss-machine-start/server/apiMeetings.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllFromDatabase } = require('./db')
+const { getAllFromDatabase, addToDatabase } = require('./db')
 const meetingsRouter = express.Router();
 
 const modelMeetings = 'meetings'
@@ -7,6 +7,17 @@ const modelMeetings = 'meetings'
 meetingsRouter.get('/', (req, res, next) => {
   const allMeetings = getAllFromDatabase(modelMeetings);
   res.send(allMeetings);
-})
+});
+
+meetingsRouter.post('/', (req, res, next) => {
+  const newMeeting = req.body;
+  
+  if(!newMeeting) {
+    res.status(400).send();
+  } else {
+    const createdMeeting = addToDatabase(modelMeetings, newMeeting)
+    res.status(201).send(createdMeeting)
+  }
+});
 
 module.exports = meetingsRouter;
