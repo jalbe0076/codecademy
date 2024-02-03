15400521cb3c8307dd42c32fa0f4a3ede8328b0c
Jason Alberto <alberto.jason@gmail.com>
2024-02-03 16:12:57 -0500

    Setup post request for /envelopes endpoint to create a new envelope

--- backend-career-path/personal-budget/server/envelopes.js
+++ backend-career-path/personal-budget/server/envelopes.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const apiEnvelopes = express.Router();
-const { envelopes } = require('./db');
+const { envelopes, createEnvelope } = require('./db');
 
 apiEnvelopes.get('/', (req, res) => {
   res.send(envelopes);
+});
+
+apiEnvelopes.post('/', (req, res) => {
+  const { title, budget, spent} = req.body;
+  if(title && budget) {
+    if(spent) {
+      const newEnvelope = createEnvelope(title, budget, spent);
+      envelopes.push(newEnvelope);
+      res.status(201).send(newEnvelope);
+    } else {
+      const newEnvelope = createEnvelope(title, budget);
+      envelopes.push(newEnvelope);
+      res.status(201).send(newEnvelope);
+    }
+  } else {
+    res.status(400).send();
+  }
 });
 
 module.exports = apiEnvelopes;
