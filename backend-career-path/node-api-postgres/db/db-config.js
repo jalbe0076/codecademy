f7792a7a0190df94ba9cac17061129377c2531d8
Jason Alberto <alberto.jason@gmail.com>
2024-02-22 14:53:54 -0500

    Add default values to the connection pool

--- backend-career-path/node-api-postgres/db/db-config.js
+++ backend-career-path/node-api-postgres/db/db-config.js
@@ -1,11 +1,11 @@
-const Pool = require('pg').Pool;
+const Pool = require('pg').Pool; // Connection pooling to maintain a connection over the course of multiple requests
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  user: process.env.DB_USER || 'me',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'api',
+  password: process.env.DB_PASSWORD || 'password',
+  port: process.env.DB_PORT || 5432
 });
 
 module.exports = pool;
