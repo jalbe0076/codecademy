2563e170b80e7480fc14245e48984d73646e332e
Jason Alberto <alberto.jason@gmail.com>
2024-02-22 15:32:34 -0500

    Adjust error handling to display additional infomation for 404 and 500 status

--- backend-career-path/node-api-postgres/db/queries.js
+++ backend-career-path/node-api-postgres/db/queries.js
@@ -1,14 +1,30 @@
 const pool = require('./db-config');
 
-const getUsers = (req, res) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (err, results) => {
-    if (err) {
-      throw err;
-    }
-    res.status(200).json(results.rows);
-  });
+const getUsers = () => {
+  return new Promise((resolve, reject) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results.rows);
+      }
+    });
+  })
+};
+
+const getUserById = (id) => {
+  return new Promise((resolve, reject) => {
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results.rows[0]);
+      }
+    });
+  })
 };
 
 module.exports = {
-  getUsers
-}
+  getUsers,
+  getUserById
+};
--- backend-career-path/node-api-postgres/server/users.js
+++ backend-career-path/node-api-postgres/server/users.js
@@ -1,7 +1,38 @@
 const express = require('express');
-const { getUsers } = require('.././db/queries');
+const { getUsers, getUserById } = require('.././db/queries');
 const apiUsers = express.Router();
 
-apiUsers.get('/', getUsers);
+apiUsers.get('/', (req, res) => {
+  getUsers()
+    .then(users => {
+      if (!users.length) {
+        res.status(404).json({ error: 'Users not found' });
+      } else {
+        res.status(200).json(users);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ err: 'Internal server error' });
+    });
+});
+
+apiUsers.get('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  getUserById(id)
+    .then(user => {
+      if(!user) {
+        res.status(404).json({ error: 'User not found' });
+      } else {
+        res.status(200).json(user);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ err: 'Internal server error' });
+    })
+
+});
 
 module.exports = apiUsers;
